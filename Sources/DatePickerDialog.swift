926a3802207c3bfe28fab359b71db71b2205fd35
Muhammad Fahmi Hidayat <67779238+fahmiITP@users.noreply.github.com>
2021-08-02 11:45:58 +0700

    Try Fix Date Picker Dialog Dark Mode Support.

--- Sources/DatePickerDialog.swift
+++ Sources/DatePickerDialog.swift
@@ -31,6 +31,7 @@ open class DatePickerDialog: UIView {
 
     private var textColor: UIColor!
     private var buttonColor: UIColor!
+    private var viewBorderColor: UIColor!
     private var font: UIFont!
 
     private var container: UIView?
@@ -38,8 +39,8 @@ open class DatePickerDialog: UIView {
 
     // MARK: - Dialog initialization
     @objc public init(
-        textColor: UIColor? = nil,
-        buttonColor: UIColor? = nil,
+        textColor: UIColor? = getDefaultTextColor(),
+        buttonColor: UIColor? = getDefaultButtonColor(),
         font: UIFont = .boldSystemFont(ofSize: 15),
         locale: Locale? = nil,
         showCancelButton: Bool = true
@@ -48,6 +49,12 @@ open class DatePickerDialog: UIView {
         super.init(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         self.textColor = textColor ?? Colors.text
         self.buttonColor = buttonColor ?? Colors.accent
+        if #available(iOS 13.0, *){
+            self.viewBorderColor =  UIColor.systemGray3
+        }
+        else{
+            self.viewBorderColor =  UIColor(red: 198/255, green: 198/255, blue: 198/255, alpha: 1)
+        }
         self.font = font
         self.showCancelButton = showCancelButton
         self.locale = locale
@@ -57,6 +64,24 @@ open class DatePickerDialog: UIView {
     @available(*, unavailable)
     @objc required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    public class func getDefaultTextColor() -> UIColor {
+        if #available(iOS 13.0, *){
+            return UIColor.label
+        }
+        else{
+            return UIColor.black
+        }
+    }
+    
+    public class func getDefaultButtonColor() -> UIColor {
+        if #available(iOS 13.0, *){
+            return UIColor.systemBlue
+        }
+        else{
+            return UIColor.blue
+        }
     }
 
     func setupView() {
@@ -190,14 +215,27 @@ open class DatePickerDialog: UIView {
 
         // First, we style the dialog to match the iOS8 UIAlertView >>>
         gradient.frame = container.bounds
-        gradient.colors = Colors.gradientBackground
+        if #available(iOS 13, *){
+             gradient.colors = [
+                 UIColor.secondarySystemBackground.cgColor,
+                 UIColor.systemBackground.cgColor,
+                 UIColor.secondarySystemBackground.cgColor
+             ]
+         }
+         else{
+             gradient.colors = [
+                 UIColor(red: 218/255, green: 218/255, blue: 218/255, alpha: 1).cgColor,
+                 UIColor(red: 233/255, green: 233/255, blue: 233/255, alpha: 1).cgColor,
+                 UIColor(red: 218/255, green: 218/255, blue: 218/255, alpha: 1).cgColor
+             ]
+         }
 
         let cornerRadius = kCornerRadius
         gradient.cornerRadius = cornerRadius
         container.layer.insertSublayer(gradient, at: 0)
 
         container.layer.cornerRadius = cornerRadius
-        container.layer.borderColor = Colors.separator.cgColor
+        container.layer.borderColor = viewBorderColor.cgColor
         container.layer.borderWidth = 1
         container.layer.shadowRadius = cornerRadius + 5
         container.layer.shadowOpacity = 0.1
@@ -217,7 +255,7 @@ open class DatePickerDialog: UIView {
             height: kDefaultButtonSpacerHeight
         ))
 
-        lineView.backgroundColor = Colors.separator
+        lineView.backgroundColor = viewBorderColor
         container.addSubview(lineView)
 
         //Title
